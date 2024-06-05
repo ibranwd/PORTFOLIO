5dec0ab5db40bb37ec4f4d2b569d7ab50e871ef1
Md Ibran <nadaf821096@gmail.com>
2024-06-05 16:35:40 +0530

    commit

---
+++ src/components/About.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+function About() {
+  return (
+    <div
+      name="About"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-16"
+    >
+      <div>
+        <h1 className="text-3xl font-bold mb-5">About</h1>
+        <p>
+          Hello, I'm Ibran, a passionate Web developer with a keen eye for MERN
+          Stack . With a background in IT, I strive to create impactful and
+          visually stunning Software solutions that leave a lasting impression.
+        </p>
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Education & Training
+        </h1>
+        <span>
+          [Course-BCA], [MAKHANLAL UNIVERSITY BHOPAL-2019-2022] 
+          [Course-MCA],[MAHARAJA RANJIT SINGH PUNJAB TECHNICAL UNIVERSITY,BATHINDA-2022-2024]
+         [FULL STACK WEB DEVELOPMENT], [PW-SKILLS],
+          [2023-2024]
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Skills & Expertise
+        </h1>
+        <span>
+          Proficient in [Programming Languages] Experienced with [Software
+          Tools/Technologies] Strong grasp of [Design Principles/Concepts]
+          Excellent problem-solving skills Effective communicator and
+          collaborator
+        </span>
+        <br />
+        
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Achievements & Awards
+        </h1>
+        <span>
+          [Award/Recognition], [Organization/Institution], [Year] [Achievement],
+          [Organization/Platform], [Year]
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Mission Statement
+        </h1>
+        <p>
+          My mission is to leverage my skills and creativity to deliver
+          innovative [WEB DEVELOPMENT] solutions that exceed client expectations and
+          contribute positively to the digital landscape. I am committed to
+          continuous learning and growth, always seeking new challenges and
+          opportunities to expand my horizons.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
